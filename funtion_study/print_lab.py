f9dfcff6da2361a726d2215ff553b0f0377938e7
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-05-11 11:37:17 +0900

    print - function\n - flush 아직도 정확히 작동하는지 모르겠음(변기가 자꾸 생각남)

---
+++ funtion_study/print_lab.py
@@ -0,0 +1,18 @@
+import sys
+filep = open("output.txt",'w') # "rw" cannot be use
+a = 100
+b = "String_B"
+print('--------Sep-----------')
+print("Defualt    -",a,b)
+print("Sep=$$$    -",a,b,sep='$$$')
+print("Sep=\_n    -",a,b,sep='\n')
+print("Sep=''     -",a,b,sep='')
+print('--------End-----------')
+print("End=''     -",a,b,end='=====\n')
+print("Next Line",end='=====\n')
+print('------End-File--------')
+print("End=''     -",a,b,end='=====\n')
+print(" Sys.stdin - type ",type(sys.stdout))
+print("End=''     -",a,b,end='=====\n',file=filep)
+print("End=''     -",a,b,end='=====\n',file=filep, flush=True) # We don't know how to using it
+filep.close()
