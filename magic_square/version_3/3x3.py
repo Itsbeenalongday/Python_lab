b247216e35dae02a5ac7d5e882ea58ca06554731
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-05-26 02:13:55 +0900

    ver1 ver2 ver3 magic square

--- magic_square/version_3/3x3.py
+++ magic_square/version_3/3x3.py
@@ -13,8 +13,11 @@
 # 짝 + 홀 + 홀 = 15
 import time
 odd = [1,3,9,7] 
-even = [8,4,2,6] 
-e = 5
+even = [8,4,2,6]
+e = 5 # 대각선 교차지점에 0~9 중간 수인 5를 넣는다. 
+a = input('디버깅 모드를 실행하려면 y 아니면 n을 입력하세요 >>> ')
+if(a == 'y'): debug = True
+else:	debug = False
 startTime = time.time()
 for i in range(4):
 	odd.insert(0,odd.pop()) # circular list
@@ -22,4 +25,8 @@ for i in range(4):
 	b,d,h,f = odd # unpack
 	a,g,i,c = even # unpack
 	print(a,b,c,d,e,f,g,h,i)
+	if(debug):
+		print(a,' ',b, ' ', c)
+		print(d,' ',e, ' ', f)
+		print(g,' ',h, ' ', i)
 print(time.time() - startTime)
