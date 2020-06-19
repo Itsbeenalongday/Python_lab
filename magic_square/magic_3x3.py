216e675bd18445387dc36989d158cb180c382239
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-06-19 17:09:32 +0900

    test nxn complete

--- magic_square/magic_3x3.py
+++ magic_square/magic_3x3.py
@@ -1,4 +1,3 @@
-
 # 0 1 2
 # 3 4 5
 # 6 7 8
@@ -7,13 +6,16 @@ from itertools import permutations
 startTime = time.time()
 answer = 0
 def count(arr):
+    # list의 모든 요소가 같은 값 평균내면 자신이 나온다.
+    global answer
+    if sum(arr)/(3*3) == arr[0]:
+        answer = 1
+        return
     for m in list(permutations(arr)): # 순열 생성
         # 마방진의 조건을 만족하면,
-        if m[0]+m[1]+m[2] == m[3]+m[4]+m[5] == m[6]+m[7]+m[8] == m[0]+m[3]+m[6] == m[1]+m[4]+m[7] == m[2]+m[5]+m[8] == m[0]+m[4]+m[8] == m[2]+m[4]+m[6]:
-            global answer
+        if sum(m[0:3]) == sum(m[3:6]) == sum(m[6:9]) == m[0]+m[3]+m[6] == m[1]+m[4]+m[7] == m[2]+m[5]+m[8] == m[0]+m[4]+m[8] == m[2]+m[4]+m[6]:
             answer += 1 # 정답 1개 추가 
 candidates = list(map(int,str.split(input('숫자들을 입력하세요 >>> ')))) # 입력 받고 공백 기준으로 parsing 후 list로 만들고,list에서 하나씩 꺼내서 int형으로 변환한다.
 count(candidates)# 알고리즘 수행
 print('총 %d개의 답이 있습니다.'%answer)#답 출력
 print('계산시간은 총' + str(time.time()-startTime) + '초 입니다')# 걸린 시간 출력
-
---
+++ magic_square/test_nxn.py
@@ -0,0 +1,44 @@
+import numpy as np
+# 마방진의 길이가 제곱수인지 판단, 제곱수가 아니면 마방진 아님
+def nextSqure(n):
+    sqrt = n ** (1/2)
+    return True if sqrt == int(sqrt) else False
+# 매개 변수로 축과 배열을 넘기고 축 기준으로 합을 구해주는
+def sum_matrix(arr, axis, options = False):
+    if options:
+        return np.sum(arr) // len(np.sum(arr, axis = axis))
+    return np.sum(arr, axis = axis)
+# \ / 두 방향
+def sum_digonal_matrix(arr, direction):
+    if direction == 0: # / 방향의 합 행렬을 열기준으로 180도 대칭하고, 대각선 합 구하면 끝
+        return np.trace(np.flip(arr))
+    else: # \ 방향의 대각선 합
+        return np.trace(arr)
+# 행과 열의 합을 구하고 나면, 그 결과들이 모두 동일한지 체크
+def check_sum_is_same(arr):
+    return True if (np.sum(arr) / len(arr)) == arr[0] else False
+# 일단 1차원의 행렬로 1xn^2으로 받고 reshape(n,n)한다. 그리고 진행
+candidates = list(map(int,str.split(input('숫자들을 입력하세요 >>> '))))
+if nextSqure(len(candidates)): # nxn이 아닌 것은 폐기처리
+    n_magic = len(candidates) ** (1/2) # n x n 마방진
+    n_magic = int(n_magic) # 결과가 실수이므로 정수로 바꾼다.
+    magic_square = np.array([]) # 빈 행렬
+    magic_square = np.append(magic_square,candidates) # 1차원으로 받음
+    magic_square = magic_square.reshape(n_magic,n_magic) # n x n 으로 변형
+    magic_square = magic_square.astype('int64') # integer로 변경
+    # 대각선 합, 행들의 합, 열들의 합을 각각 구하고 모두 같은지 확인
+    print(sum_matrix(magic_square,1))
+    if(check_sum_is_same(sum_matrix(magic_square,1))): # 행
+        if(check_sum_is_same(sum_matrix(magic_square,0))): # 열
+            if(sum_digonal_matrix(magic_square,0) == sum_digonal_matrix(magic_square,1)): # 두 대각선
+                if(sum_digonal_matrix(magic_square,0) == sum_matrix(magic_square,0,True) == sum_matrix(magic_square,1,True)): # 두 대각선 = 행의 합 = 열의 합 체크
+                    print('정답입니다.') 
+            else:
+                print('정답이 아닙니다.')
+        else:
+            print('정답이 아닙니다')
+    else:
+        print('정답이 아닙니다.')
+else:
+    print('마방진의 형태가 아닙니다.')
+
